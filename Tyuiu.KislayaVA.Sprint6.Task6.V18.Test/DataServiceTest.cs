5dbd18f36249397bf1f7a7d19235e682e1a6c271
Вера Кислая < @DESKTOP-LROD2HO>
2025-12-26 20:26:11 +0500

    Task6

---
+++ Tyuiu.KislayaVA.Sprint6.Task6.V18.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.KislayaVA.Sprint6.Task6.V18.Lib;
+namespace Tyuiu.KislayaVA.Sprint6.Task6.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\Вера Кислая\source\repos\Tyuiu.KislayaVA.Sprint6\Files\TestDataTask6";
+            DataService ds = new DataService();
+            string res = ds.CollectTextFromFile(path);
+            string wait = "an bbbn n aaaaaaaaaaaaan";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
