ea82a512f4ab087da6e03ea6c4966f1e9bc17bb8
Вера Кислая < @DESKTOP-LROD2HO>
2025-12-26 21:25:03 +0500

    Task2 + solution file

--- Tyuiu.KislayaVA.Sprint6.Task2.V12.Test/DataServiceTest.cs
+++ Tyuiu.KislayaVA.Sprint6.Task2.V12.Test/DataServiceTest.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.KislayaVA.Sprint6.Task2.V12.Test
         public void TestMethod1()
         {
             DataService ds = new DataService();
-            Assert.AreEqual(new double[] { 2.19, 3.31, 2.97, 1.18, 0.03 }, ds.GetMassFunction(1, 5));
+            CollectionAssert.AreEqual(new double[] { 2.19, 3.31, 2.97, 1.18, 0.03 }, ds.GetMassFunction(1, 5));
         }
     }
 }
