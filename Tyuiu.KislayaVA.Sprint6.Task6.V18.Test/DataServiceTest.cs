27d901f1dfdc8b5eb12559473a882f317c62cd61
Вера Кислая < @DESKTOP-LROD2HO>
2025-12-26 21:23:31 +0500

    Task6 fixes

--- Tyuiu.KislayaVA.Sprint6.Task6.V18.Test/DataServiceTest.cs
+++ Tyuiu.KislayaVA.Sprint6.Task6.V18.Test/DataServiceTest.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.KislayaVA.Sprint6.Task6.V18.Test
         [TestMethod]
         public void TestMethod1()
         {
-            string path = @"C:\Users\Вера Кислая\source\repos\Tyuiu.KislayaVA.Sprint6\Files\TestDataTask6";
+            string path = @"C:\Users\Вера Кислая\source\repos\Tyuiu.KislayaVA.Sprint6\Files\TestDataTask6.txt";
             DataService ds = new DataService();
             string res = ds.CollectTextFromFile(path);
             string wait = "an bbbn n aaaaaaaaaaaaan";
