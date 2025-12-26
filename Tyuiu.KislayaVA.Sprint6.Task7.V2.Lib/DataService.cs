2bc8b0855e3dcca3091b8374d0e05831844c3d74
Вера Кислая < @DESKTOP-LROD2HO>
2025-12-26 21:24:04 +0500

    Task7

---
+++ Tyuiu.KislayaVA.Sprint6.Task7.V2.Lib/DataService.cs
@@ -0,0 +1,37 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.KislayaVA.Sprint6.Task7.V2.Lib
+{
+    public class DataService : ISprint6Task7V2
+    {
+        public int[,] GetMatrix(string path)
+        {
+            string fileData = File.ReadAllText(path);
+            fileData = fileData.Replace('\n', '\r');
+            string[] lines = fileData.Split(new char[] { '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int rows = lines.Length;
+            int columns = lines[0].Split(';').Length;
+
+            int[,] matrix = new int[rows, columns];
+
+            for (int i = 0; i < rows; i++)
+            {
+                string[] line_i = lines[i].Split(';');
+                for (int j = 0; j < columns; j++)
+                {
+                    matrix[i, j] = Convert.ToInt32(line_i[j]);
+                }
+            }
+
+            for (int j = 0; j < rows; j++)
+            {
+                if (Math.Abs(matrix[1, j] % 2) == 0)
+                {
+                    matrix[1, j] = 555;
+                }
+            }
+            return matrix;
+        }
+    }
+}
---
+++ Tyuiu.KislayaVA.Sprint6.Task7.V2.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.KislayaVA.Sprint6.Task7.V2.Lib;
+namespace Tyuiu.KislayaVA.Sprint6.Task7.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+
+            string path = @"C:\Users\Вера Кислая\source\repos\Tyuiu.KislayaVA.Sprint6\Files\TestDataTask7.csv";
+            string fileData = File.ReadAllText(path);
+            int[,] result = ds.GetMatrix(path);
+            int[,] wait =
+            {
+                {1, 1, 2, 3, 4},
+                {555, 1, 555, 1, 555},
+                {1, 1, 1, 1, 1},
+                {2, 4, 4, 4, 2},
+                {5, 1, 3, 5, 2}
+
+            };
+            CollectionAssert.AreEqual(wait, result);
+        }
+    }
+}
